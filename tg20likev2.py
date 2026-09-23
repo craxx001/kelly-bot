47ef49067e2f6faf5c54fb234f349abae21c8d23
craxx001 <raviowo1@gmail.com>
2026-09-23 06:10:35 +0530

    Update tg20likev2.py

--- tg20likev2.py
+++ tg20likev2.py
@@ -19,7 +19,7 @@ API_KEY = os.getenv("API_KEY", "CRAXX")
 
 # IMPORTANT:
 # Set BOT_TOKEN as an environment variable on your hosting platform.
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+BOT_TOKEN = os.getenv("8617664721:AAFzgh7TwfJPhAW4JZtlnVAIoiSw6WRx1ZA")
 
 # Example: ADMIN_IDS=8853678390,123456789
 ADMIN_IDS = [
